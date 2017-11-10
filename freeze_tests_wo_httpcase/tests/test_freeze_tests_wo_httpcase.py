e868cb27b3b6c3256355180cf71b3b1bf8def80a
Moisés López <moylop260@vauxoo.com>
2017-11-10 17:42:03 -0600

    [REF] freeze_tests_wo_httpcase: Add comments

--- freeze_tests_wo_httpcase/tests/test_freeze_tests_wo_httpcase.py
+++ freeze_tests_wo_httpcase/tests/test_freeze_tests_wo_httpcase.py
@@ -13,11 +13,12 @@ except ImportError:
 @odoo.tests.post_install(True)
 class TestNoFreezeWHttpCase(odoo.tests.HttpCase):
     def test_open_url(self):
-        "Testing that open a non-static url with HttpCase is good!"
+        "Testing that opening a non-static url with HttpCase is good!"
         urlopen('http://localhost:8069/web/login')
 
 
 class TestFreezeWoHttpCase(odoo.tests.TransactionCase):
     @unittest.skip("Disable it if you want to see a testing freeze")
     def test_open_url(self):
+        "Testing that before start odoo entirely is not available to use non-static url without HttpCase"
         urlopen('http://localhost:8069/web/login')
